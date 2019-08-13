8ac975492c16251d6adc2bb3f9c378977f07f940
zmink <zmink@phoenixcon.com>
2019-08-12 19:27:44 -0700

    Started css module integration.  Added withKnobs support

--- src/components/AXL_LED/AXL_LED.jsx
+++ src/components/AXL_LED/AXL_LED.jsx
@@ -1,5 +1,7 @@
 import {ReactComponent as LED} from  '../../protocol-icons/icons/devices/svg/axlLED.svg';
 import React from 'react';
+import classNames from 'classnames';
+import styles from './AXL_LED.module.css'
 
 export default class AXL_LED extends React.PureComponent {
     constructor(props){
@@ -7,6 +9,13 @@ export default class AXL_LED extends React.PureComponent {
     }
 
     render(){
-        return <LED></LED>
+        var ledClass = classNames(styles.led_base,{[styles.error]:this.props.isError},{[styles.warning]:this.props.isWarning},{[styles.active]:this.props.isActive})
+        return <LED
+            className={ledClass}
+        ></LED>
     }
 }
+
+//isError
+//isWarning
+//isActive
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -3,10 +3,14 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 
 import { Button, Welcome } from '@storybook/react/demo';
 
 import AXL_LED from '../src/components/AXL_LED/AXL_LED.jsx';
+
+//const stories = storiesOf('Storybook Knobs', module);
+
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -21,7 +25,12 @@ storiesOf('Button', module)
   ));
 
   storiesOf('AXL_LED', module)
+  .addDecorator(withKnobs)
   .add('with text', () => <AXL_LED/>)
+  .add('as dynamic variables', () => {
+    
+    return (<AXL_LED isError={boolean('isError', false)} isWarning={boolean('isWarning', false)} isActive={boolean('isActive', false)}/>);
+  })
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">

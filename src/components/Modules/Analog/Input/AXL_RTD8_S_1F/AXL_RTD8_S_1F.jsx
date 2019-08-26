eea3898a84650ed706917ab6aa25ab1299e96936
Zachary Mink <zmink@phoenixcon.com>
2019-08-26 08:37:37 -0400

    Added rtd 8 s component

---
+++ src/components/Modules/Analog/Input/AXL_RTD8_S_1F/AXL_RTD8_S_1F.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import AXL_Module,{defaultTerminals,defaultLeds} from '../../../../AXL_Module/AXL_Module.jsx';
+import AXL_Connector from '../../../../AXL_Connector/AXL_Connector.jsx';
+
+
+export default class AXL_RTD8_S_1F extends React.PureComponent {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <AXL_Module partName={this.props.partName} width={54} terminals={this.props.terminals} leds={this.props.leds} raisedMidsection={false} hasTopRow={false} partNumber={this.props.partNumber} labels={['a','0','1','2','3','4','5','6','7']} colorCard={{isAnalogIn:true}}>
+                <AXL_Connector type="POWER" isBlack={true} numRows={4} numCols={1}/>
+                <AXL_Connector type="IO" isBlack={false} numRows={4} numCols={2}/>
+                <AXL_Connector type="IO" isBlack={false} numRows={4} numCols={2}/>
+                <AXL_Connector type="IO" isBlack={false} numRows={4} numCols={2}/>
+                <AXL_Connector type="IO" isBlack={false} numRows={4} numCols={2}/>
+            </AXL_Module>
+        )
+    }
+}
+
+AXL_RTD8_S_1F.defaultProps = {
+    partName:"RTD8 S",
+    partNumber: "2702120",
+    terminals: defaultTerminals(12),
+    leds: defaultLeds(12)
+}

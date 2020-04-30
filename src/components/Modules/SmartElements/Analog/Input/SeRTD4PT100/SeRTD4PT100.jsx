9a7aba45c55928431be8b8cf994ae6c3a782da21
zmink-pxc <zmink@phoenixcontact.com>
2020-04-29 17:04:58 -0700

    implemented rtd 4 component

---
+++ src/components/Modules/SmartElements/Analog/Input/SeRTD4PT100/SeRTD4PT100.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SeModule from '@core/SeModule/SeModule.jsx';
+import SeConnector from '@core/SeConnector/SeConnector.jsx';
+
+export default function SeRTD4PT100(props){
+    const {partName,partNumber,ledLabels,...other} = props;
+    return <SeModule partName={partName} partNumber={partNumber} ledLabels={ledLabels} {...other}><SeConnector/></SeModule>
+}
+
+SeRTD4PT100.defaultProps = {
+    partName: "RTD4",
+    partNumber: "1088106",
+    ledLabels: ['E','','','','','','','','','','','','','','','','','E']
+}

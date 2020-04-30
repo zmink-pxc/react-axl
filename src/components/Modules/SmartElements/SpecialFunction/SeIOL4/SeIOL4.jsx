df8783addcb51ed6a72e876cde9173b1f4ff624c
zmink-pxc <zmink@phoenixcontact.com>
2020-04-29 21:42:18 -0700

    added IOL component and fixed name of SeINC comp

--- src/components/Modules/SmartElements/SpecialFunction/SeIOL4/SeIOL4.jsx
+++ src/components/Modules/SmartElements/SpecialFunction/SeIOL4/SeIOL4.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import SeModule from '@core/SeModule/SeModule.jsx';
 import SeConnector from '@core/SeConnector/SeConnector.jsx';
 
-export default function SeINC1SYM(props){
+export default function SeIOL4(props){
     const {partName,partNumber,ledLabels,...other} = props;
     return <SeModule partName={partName} partNumber={partNumber} ledLabels={ledLabels} {...other}><SeConnector/><SeConnector/></SeModule>
 }
 
-SeINC1SYM.defaultProps = {
-    partName: "INC1",
-    partNumber: "1088130",
-    ledLabels: ['E','POS','NEG','','','','','UE','','','REF','L','','','','','','E']
+SeIOL4.defaultProps = {
+    partName: "IOL4",
+    partNumber: "1088132",
+    ledLabels: ['E','CQ1','','','','CQ2','','','','CQ3','','','','CQ4','','','','E']
 }

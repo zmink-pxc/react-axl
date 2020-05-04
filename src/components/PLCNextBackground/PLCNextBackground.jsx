18ea521c9e676da07d73fabf31b0e7eb89ce78e0
zmink-pxc <zmink@phoenixcontact.com>
2020-05-04 08:19:48 -0700

    code cleanup

--- src/components/PLCNextBackground/PLCNextBackground.jsx
+++ src/components/PLCNextBackground/PLCNextBackground.jsx
@@ -1,7 +1,7 @@
 import React from './node_modules/react';
-import styles from "./PLCNextBackground.module.scss";
-import classNames from './node_modules/classnames';
-import Particles from './node_modules/react-particles-js';
+import styles from "./PLCnextBackground.module.scss";
+import classNames from 'classnames';
+import Particles from 'react-particles-js';
 import particles from './particles.json';
 
 export default class PLCnextBackground extends React.PureComponent {
@@ -13,31 +13,6 @@ export default class PLCnextBackground extends React.PureComponent {
         particles.particles.color.value = [styles.colorPistachio,styles.colorNeptune,styles.colorDownriver,styles.colorWaterloo,styles.colorAlto]
         return <div>
             <Particles params={particles}/>
-            {/* <Particles params={particles2}/> */}
-            {/* <Particles params={particles3}/> */}
         </div>
     }
-    // render(){
-    //     const foregroundElements = [];
-    //     const backgroundElements = [];
-    //     for (var i=0;i<this.props.numForegroundElements;i++){
-    //         const cInd = i % styles.classCount;
-    //         const feClass = classNames(styles["foreground-element-"+cInd],styles.foregroundMove)
-    //         foregroundElements.push(<div className={feClass}></div>)
-    //     }
-    //     for (var i=0;i<this.props.numBackgroundElements;i++){
-    //         const cInd = i % styles.classCount;
-    //         const bgClass = classNames(styles["background-element-"+cInd],styles.backgroundMove)
-    //         backgroundElements.push(<div className={bgClass}></div>)
-    //     }
-
-    //     return (<div className={this.props.className} style={{backgroundColor: styles.backgroundDark,width:'100vw',height:'100vh',...this.props.style}}>
-    //         <div className={styles.foreground}>
-    //             {foregroundElements}
-    //         </div>
-    //         <div className={styles.background}>
-    //             {backgroundElements}
-    //         </div>
-    //     </div>)
-    // }
 }

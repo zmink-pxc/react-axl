e1903d5912666db2641d826ed084de6fc44e386a
zmink-pxc <zmink@phoenixcontact.com>
2020-05-23 09:36:49 -0700

    Started snapshot test coverage

---
+++ __tests__/AxlModule.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import AxlModule from '../src/components/Core/AxlModule/AxlModule.jsx';
+import AxlConnector from '../src/components/Core/AxlConnector/AxlConnector.jsx';
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  var component = renderer.create(<AxlModule/>)
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  component = renderer.create(<AxlModule partName="AI2 AO2 XC" width={35} raisedMidsection={false} hasTopRow={false} partNumber="1035429" labels={['a','0','1','2','3']} colorCard={{isAnalogIn:true}}>
+      <AxlConnector type="POWER" isBlack={true} numRows={4} numCols={1}/>
+      <AxlConnector type="IO" isBlack={false} numRows={4} numCols={2}/>
+      <AxlConnector type="IO" isBlack={false} numRows={4} numCols={2}/>
+      </AxlModule>);
+
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  
+});

3c58779e0efaafe2b6a34daa398c7116c159c21e
zmink-pxc <zmink@phoenixcontact.com>
2020-07-27 13:20:33 -0700

    Updated axl plunger imports

--- src/components/Core/AxlPlunger/AxlPlunger.jsx
+++ src/components/Core/AxlPlunger/AxlPlunger.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
-import styles from './AxlPlunger.module.scss'
+import styles from '@bit/zmink.axioline.axl-plungermodule';
 
 export default class AxlPlunger extends React.PureComponent {
     constructor(props){

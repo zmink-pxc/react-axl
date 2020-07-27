a261b1af1ed4bade152b1c2dde935c37d226ca84
zmink-pxc <zmink@phoenixcontact.com>
2020-07-27 13:06:36 -0700

    Updated imports for se carrier terminal

--- src/components/Core/SeCarrierTerminal/SeCarrierTerminal.jsx
+++ src/components/Core/SeCarrierTerminal/SeCarrierTerminal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import styles from './SeCarrierTerminal.module.scss'
+import styles from '@bit/zmink.axioline.smart-elements.se-carrier-terminal-module';
 
 export default class SeCarrierTerminal extends React.PureComponent {
     constructor(props){
